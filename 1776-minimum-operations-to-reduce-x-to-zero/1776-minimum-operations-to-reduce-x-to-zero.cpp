4ccea8049232f2431610917e41747e5cab79140e
RupanugaPM <92666237+RupanugaPM@users.noreply.github.com>
2023-09-20 10:06:17 +0530

    Time: 400 ms (12.15%), Space: 172.9 MB (7.56%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int totalSum = 0;
-        for (int num : nums)
-            totalSum += num;
-        int maxLength = -1, currSum = 0;
-        for (int l = 0, r = 0; r < nums.size(); r++) {
-            currSum += nums[r];
-            while (l <= r && currSum > totalSum - x)
-                currSum -= nums[l++];
-            if (currSum == totalSum - x)
-                maxLength = max(maxLength, r - l + 1);
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        vector<int>suf(n,0);
+        suf[n-1]=nums[n-1];
+        mp[suf[n-1]]=1;
+        for(int i=n-2;i>=0;i--){
+            suf[i]=suf[i+1]+nums[i];
+            mp[suf[i]]=n-i;
         }
-        return maxLength == -1 ? -1 : nums.size() - maxLength;
+        int pre=0;
+        int ans=nums.size()+1;
+        if(mp.find(x)!=mp.end()){
+            ans=min(ans,mp[x]);
+        }
+        for(int i=0;i<n;i++){
+            pre+=nums[i];
+            mp.erase(nums[i]);
+            if(x-pre==0){
+                ans=min(ans,i+1);
+            }
+            if(mp.find(x-pre)!=mp.end()){
+                ans=min(ans,i+1+mp[x-pre]);
+            }
+        }
+        if(ans==n+1){
+            return -1;
+        }
+        return ans;
     }
 };

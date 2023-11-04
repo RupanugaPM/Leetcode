a6f3ec27111a2c2c0824c23a3be719c2199a40b1
RupanugaPM <92666237+RupanugaPM@users.noreply.github.com>
2023-11-04 12:36:03 +0530

    Time: 11 ms (97.41%), Space: 23.1 MB (59.05%) - LeetHub

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -1,10 +1,13 @@
+#pragma GCC optimise("O3")
 class Solution {
 public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         int ans=0;
+        #pragma unroll
         for(auto i:left){
             ans=max(ans,i);
         }
+        #pragma unroll
         for(auto i:right){
             ans=max(ans,n-i);
         }
